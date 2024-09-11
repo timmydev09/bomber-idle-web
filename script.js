6080910e5a6cde8973297dfebcbbccaaf19d13f2
hoatruong <hoatruongdev09@gmail.com>
2024-09-11 16:34:39 +0700

    update build

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/development.loader.js?638605499711059385";
+var loaderUrl = buildUrl + "/development.loader.js?638616690626819198";
 var config = {
     dataUrl: buildUrl + "/development.data",
     frameworkUrl: buildUrl + "/development.framework.js",
