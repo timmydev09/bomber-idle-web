abb2d7c153a9b26594829d0c9200c5ed5ace37ce
hoatruong <hoatruongdev09@gmail.com>
2025-02-04 10:52:05 +0700

    update build

--- JsBridge/main.js
+++ JsBridge/main.js
@@ -1,9 +1,9 @@
-import telegramModule from './modules/telegramModule.js?638742624217413456';
-import unityModule from './modules/unityModule.js?638742624217413456';
-import tonConnectModule from './modules/tonConnectModule.js?638742624217413456';
-import tonWebModule from './modules/tonWebModule.js?638742624217413456';
-import adsgramModule from './modules/adsgramModule.js?638742624217413456';
-import worldAppModule from './modules/worldAppModule.js?638742624217413456';
+import telegramModule from './modules/telegramModule.js?638742629822321917';
+import unityModule from './modules/unityModule.js?638742629822321917';
+import tonConnectModule from './modules/tonConnectModule.js?638742629822321917';
+import tonWebModule from './modules/tonWebModule.js?638742629822321917';
+import adsgramModule from './modules/adsgramModule.js?638742629822321917';
+import worldAppModule from './modules/worldAppModule.js?638742629822321917';
 
 window.telegramModule = telegramModule;
 window.unityModule = unityModule;
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/development.loader.js?638742624217413456";
+var loaderUrl = buildUrl + "/development.loader.js?638742629822321917";
 var config = {
-    dataUrl: buildUrl + "/9e4ab2f2cfdd30ed61edc01e1966dc50.data.unityweb",
-    frameworkUrl: buildUrl + "/e893b1380ba4510aba56af6cc468d363.js.unityweb",
-    codeUrl: buildUrl + "/6b1fd53dd5e4504a2d418726377d3a2c.wasm.unityweb",
+    dataUrl: buildUrl + "/88f1d379ad1dea6c5df91067b80de278.data.unityweb",
+    frameworkUrl: buildUrl + "/e79cf7e4e39246b766dbffb4cb235099.js.unityweb",
+    codeUrl: buildUrl + "/d5fd8f2fda1f866b463e5ad66bb055b7.wasm.unityweb",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "DefaultCompany",
     productName: "bomb-idle",
